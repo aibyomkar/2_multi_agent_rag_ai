e408468361cc40d37c663d6d6588de7110405405
aibyomkar <omkarpotdar42@gmail.com>
2025-07-23 02:24:37 +0530

    c

--- Pdfassistant/pdf_assistant.py
+++ Pdfassistant/pdf_assistant.py
@@ -15,3 +15,37 @@ knowledge_base = PDFUrlKnowledgeBase(
     urls=["https://phi-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf"],
     vector_db=PgVector2(collection="recipes",db_url=db_url),
     )
+
+knowledge_base.load()
+
+storage = PgAssistantStorage(table_name='pdf_assistant', db_url=db_url)
+
+def pdf_assistant(new: bool = False, user: str = 'user'):
+
+    run_id: Optional[str] = None
+    if not new:
+        existing_run_ids: List[str] = storage.get_all_ids(user)
+
+        if len(existing_run_ids) > 0:
+            run_id = existing_run_ids[0]
+
+    assistant = Assistant(
+        run_id = run_id,
+        user_id = user,
+        knowledge_base = knowledge_base,
+        storage = storage,
+        # show tool calls in the response
+        show_tool_calls = True,
+        # enable the assistant to search the knowledge base
+        search_knowledge=True,
+        # enable the assistant to read thechat history
+        read_chat_history=True
+    )
+
+    if run_id is None:
+        run_id = assistant.run_id
+        print(f'Started Run: {run_id}\n')
+    else:
+        print(f'Continuing Run: {run_id}\n')
+
+    assistant.cli_app(markdown = True)
